ab4b3d09ff247213f27ae852bfed9524f77467ad
11 <11>
2020-08-31 09:40:58 +0800

    jenkins文件的修改

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#REGISTRY=registry.cn-hangzhou.aliyuncs.com
-REPOSITORY=wuzhike/paperless-meeting-system  # This is the one mostly we need to change for different project
+REGISTRY=registry.cn-hangzhou.aliyuncs.com
+REPOSITORY=$REGISTRY/wuzhike/paperless-meeting-system  # This is the one mostly we need to change for different project
 TAG=latest
 
 cd /home/sino-box/paperless-meeting-system
